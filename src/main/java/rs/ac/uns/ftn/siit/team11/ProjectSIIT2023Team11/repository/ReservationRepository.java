591b2c308a99db24e72b1647255566ffa4ab5c66
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2023-11-21 23:07:43 +0100

    feat: Added repository methods

--- src/main/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/repository/ReservationRepository.java
+++ src/main/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/repository/ReservationRepository.java
@@ -1,4 +1,45 @@
 package rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11.repository;
 
-public class ReservationRepository {
+import org.springframework.stereotype.Repository;
+import rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11.domain.Reservation;
+
+import java.util.Collection;
+
+@Repository
+public class ReservationRepository implements  IReservationRepository{
+
+    @Override
+    public Collection<Reservation> findAll() {
+        return null;
+    }
+
+    @Override
+    public Collection<Reservation> findByUserEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public Collection<Reservation> findByAccommodationId(int accommodationId) {
+        return null;
+    }
+
+    @Override
+    public Reservation findById(int reservationId) {
+        return null;
+    }
+
+    @Override
+    public Reservation create(Reservation reservation) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Reservation update(Reservation reservation) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void delete(int reservationId) {
+
+    }
 }
