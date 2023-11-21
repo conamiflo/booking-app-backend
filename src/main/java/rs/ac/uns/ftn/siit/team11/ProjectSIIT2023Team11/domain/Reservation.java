e68ca8eee19dd59db8680990e668b3a54c7cf1fe
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2023-11-21 23:03:28 +0100

    feat: Created controller,service and repository for reservations

--- src/main/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/domain/Reservation.java
+++ src/main/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/domain/Reservation.java
@@ -1,7 +1,89 @@
 package rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11.domain;
 
+import java.time.LocalDate;
+
 public class Reservation {
-    private Long reservationId;
-    private Long accommodationId;
-    private Long guestId;
+    private int id;
+    private int accommodation;
+    private String user;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int numberOfGuests;
+    private String status;
+    private Double price;
+
+    public Reservation(int id, int accommodation, String user, LocalDate startDate, LocalDate endDate, int numberOfGuests, String status, Double price) {
+        this.id = id;
+        this.accommodation = accommodation;
+        this.user = user;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.numberOfGuests = numberOfGuests;
+        this.status = status;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccommodation() {
+        return accommodation;
+    }
+
+    public void setAccommodation(int accommodation) {
+        this.accommodation = accommodation;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
