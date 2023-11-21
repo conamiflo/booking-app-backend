0b0bcc9a93b211a8489eb4625df8e8ee647d46f2
OgnjenGligoric <87246330+OgnjenGligoric@users.noreply.github.com>
2023-11-21 14:07:58 +0100

    feat: project init

---
+++ src/main/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/ProjectSiit2023Team11Application.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSiit2023Team11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSiit2023Team11Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/ProjectSiit2023Team11ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSiit2023Team11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
