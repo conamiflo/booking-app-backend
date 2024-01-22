8a0fc1e9c842e52dd62b78b9cdc32a8952e48694
OgnjenGligoric <gligoric383@gmail.com>
2024-01-22 02:23:07 +0100

    feat: e2e all cases implemented

--- src/test/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/e2e/pages/HomePage.java
+++ src/test/java/rs/ac/uns/ftn/siit/team11/ProjectSIIT2023Team11/e2e/pages/HomePage.java
@@ -1,16 +1,18 @@
 package rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11.e2e.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.w3c.dom.ranges.Range;
 import rs.ac.uns.ftn.siit.team11.ProjectSIIT2023Team11.domain.Accommodation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class HomePage {
@@ -30,8 +32,36 @@ public class HomePage {
     @FindBy(css="#my-accommodations")
     private WebElement myAccommodationsButton;
 
+    @FindBy(xpath = "//input[@type='date' and contains(@placeholder, 'Check in')]")
+    private WebElement checkInDate;
+    @FindBy(xpath = "//input[@type='date' and contains(@placeholder, 'Check out')]")
+    private WebElement checkOutDate;
+
+    @FindBy(xpath = "//input[@type='text' and contains(@placeholder, 'Location')]")
+    private WebElement locationInput;
+
+    @FindBy(xpath = "//input[@type='range' and @min='1' and @max='12']")
+    private WebElement numberOfGuestsRange;
+
+    @FindBy(xpath = "//img[@alt='Vector1328']")
+    private WebElement searchImage;
+
+    @FindBy(xpath = "//div[@class ='cards-matbtn2' and img[@alt='Vector1328']]")
+    private WebElement searchContainer;
     @FindBy(css=".accommodation-card-container")
     private List<WebElement> accommodationCardContainers;
+
+    @FindBy(xpath = "//div[@class='cards-filtersmatinputslot']")
+    private WebElement filterContainerButton;
+
+    @FindBy(xpath = "//span[@class='mdc-button__label' and text()='Apply']")
+    private WebElement applyButton;
+
+
+    @FindBy(xpath = "//input[@id='mat-input-0']")
+    private WebElement minimumPriceInputElement;
+    @FindBy(xpath = "//input[@id='mat-input-1']")
+    private WebElement maximumPriceInputElement;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -90,5 +120,85 @@ public class HomePage {
     }
 
 
+    public void setCheckInDate(String date) {
+        setInputValue(checkInDate, date);
+    }
 
+    public long getCheckInDateInSeconds() throws ParseException {
+        String date = checkInDate.getAttribute("value");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date parsedDate = dateFormat.parse(date);
+        return parsedDate.getTime() / 1000;
+    }
+
+    public void setCheckOutDate(String date) {
+        setInputValue(checkOutDate, date);
+    }
+
+    public long getCheckOutDateInSeconds() throws ParseException {
+        String date = checkInDate.getAttribute("value");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date parsedDate = dateFormat.parse(date);
+        return parsedDate.getTime() / 1000;
+    }
+
+    public void setLocation(String location) {
+        setInputValue(locationInput, location);
+    }
+    public String getLocation() {
+        return locationInput.getAttribute("value");
+    }
+    public void setNumberOfGuests(Integer value) {
+
+        for(int i = 0; i < 12; i ++){
+            numberOfGuestsRange.sendKeys(Keys.ARROW_LEFT);
+        }
+        for(int i = 1; i < value; i ++){
+            numberOfGuestsRange.sendKeys(Keys.ARROW_RIGHT);
+        }
+    }
+    public int getNumberOfGuests() {
+        String value = numberOfGuestsRange.getAttribute("value");
+        return Integer.parseInt(value);
+    }
+    private void setInputValue(WebElement inputElement, String value) {
+        inputElement.clear();
+        inputElement.sendKeys(value);
+    }
+
+    public void setMaximumPrice(Integer value) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute('disabled');", maximumPriceInputElement);
+        maximumPriceInputElement.clear();
+        maximumPriceInputElement.sendKeys(String.valueOf(value));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('disabled', '');", maximumPriceInputElement);
+    }
+
+    public String getMinimumPrice() {
+        return minimumPriceInputElement.getAttribute("value");
+    }
+
+    public void setMinimumPrice(Integer value) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute('disabled');", minimumPriceInputElement);
+        minimumPriceInputElement.clear();
+        minimumPriceInputElement.sendKeys(String.valueOf(value));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('disabled', '');", minimumPriceInputElement);
+    }
+
+    public String getMaximumPrice() {
+        return maximumPriceInputElement.getAttribute("value");
+    }
+
+    public void clickOnSearchContainer() {
+        searchContainer.click();
+    }
+    public void clickOnFilterContainer() {
+        filterContainerButton.click();
+    }
+    public void clickOnApplyContainer() {
+        applyButton.click();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5L));
+        wait.until(ExpectedConditions.invisibilityOf(maximumPriceInputElement));
+    }
 }
